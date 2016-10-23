4b1f0f19e81a1cb025baf38ae7c9e670ba9773d0
cly715 <1114130065@qq.com>
2016-10-23 17:18:59 +0800

    第二次

--- app/src/main/java/com/qf/xs/day30screenadpt/MainActivity.java
+++ app/src/main/java/com/qf/xs/day30screenadpt/MainActivity.java
@@ -1,13 +1,14 @@
 package com.qf.xs.day30screenadpt;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+//import android.support.v7.widget.Toolbar;
+//import android.view.View;
+//import android.view.Menu;
+//import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
 
+        TextView textView = (TextView) findViewById(R.id.text_tv);
+        textView.setText("dasda");
     }
 
 
